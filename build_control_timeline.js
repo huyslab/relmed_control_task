2bc2c8d17963042ed05e812143db8c54b6e5ee21
Haoyang Lu <haoyang.lu@ucl.ac.uk>
2025-03-11 11:49:30 +0000

    Fix no responding in controlTotalReward

--- build_control_timeline.js
+++ build_control_timeline.js
@@ -137,18 +137,19 @@ let controlTotalReward = {
   type: jsPsychHtmlKeyboardResponse,
   stimulus: function () {
     let total_bonus = jsPsych.data.get().filter({ trialphase: 'control_reward_feedback' }).select('correct').sum() * 5 / 100;
-    return `<main class="main-stage">
+    return `
+        <main class="main-stage">
           <img class="background" src="imgs/ocean_above.png" alt="Background"/>
           <div class="instruction-dialog" style="bottom:50%; min-width: 600px; width: 50%;">
             <div class="instruction-content" style="font-size: 32px; text-align: center;">
-              <p>You total earnings from the shipping quests are ${total_bonus.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' })}.!</p>
+              <p>You total earnings from the shipping quests are ${total_bonus.toLocaleString('en-GB', { style: 'currency', currency: 'GBP' })}!</p>
               <p>Thank you for playing the game!</p>
               <p>Now press any key to continue.</p>
             </div>
           </div>
         </main>`;
   },
-  choices: ['ALL_KEYS'],
+  choices: "ALL_KEYS",
   response_ends_trial: true,
   post_trial_gap: 800,
   data: { 
